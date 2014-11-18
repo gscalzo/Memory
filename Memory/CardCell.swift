37b4d10e604adc205b35cd66a3c84bb8b475d2cf
giordano scalzo <giordano.scalzo@gmail.com>
2014-11-18 22:15:54 +0000

    First import

---
+++ Memory/CardCell.swift
@@ -0,0 +1,28 @@
+//
+//  CardCell.swift
+//  Memory
+//
+//  Created by Giordano Scalzo on 18/11/2014.
+//  Copyright (c) 2014 Swift by Example. All rights reserved.
+//
+
+import UIKit
+
+class CardCell: UICollectionViewCell {
+    let imageView: UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
+//        imageView.contentMode = UIViewContentMode.ScaleAspectFit
+        contentView.addSubview(imageView)
+        contentView.backgroundColor = UIColor.clearColor()
+        
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
---
+++ Memory/MemoryViewController.swift
@@ -0,0 +1,55 @@
+//
+//  MemoryViewController.swift
+//  Memory
+//
+//  Created by Giordano Scalzo on 18/11/2014.
+//  Copyright (c) 2014 Swift by Example. All rights reserved.
+//
+
+import UIKit
+
+class MemoryViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    var collectionView: UICollectionView?
+    var cellColor = false
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.greenColor()
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+        layout.itemSize = CGSize(width: 60, height: 60*1.452)
+        layout.minimumLineSpacing = 5
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView!.dataSource = self
+        collectionView!.delegate = self
+        collectionView!.registerClass(CardCell.self, forCellWithReuseIdentifier: "cardCell")
+        collectionView!.backgroundColor = UIColor.whiteColor()
+        self.view.addSubview(collectionView!)
+        collectionView!.backgroundColor = UIColor.greenColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 24
+    }
+    
+     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier("cardCell", forIndexPath: indexPath) as CardCell
+        cell.backgroundColor = UIColor.clearColor()
+       
+        cell.imageView.image = UIImage(named: "back")
+        
+        return cell
+    }
+
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+}
+
